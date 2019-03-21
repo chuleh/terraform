e80269e81a6342c068e86cc1d9ef3f108b30c867
chuleh <lcasarettom@gmail.com>
2019-03-21 10:47:49 -0300

    demo-1

---
+++ demo-1/vars.tf
@@ -0,0 +1,11 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-020a9a7369c26052b"
+  }
+}
